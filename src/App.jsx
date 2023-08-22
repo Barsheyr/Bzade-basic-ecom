01e9538389b582294aadf298c840c05e4a2f71ae
barsheyr <barsheyr@gmail.com>
2023-08-22 18:03:56 +0100

    Pages to page in path

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./Pages/Home";
-import ProductDetails from "./Pages/ProductDetails";
+import Home from "./pages/Home";
+import ProductDetails from "./pages/ProductDetails";
 
 import Sidebar from "./components/Sidebar";
 import Header from "./components/Header";
